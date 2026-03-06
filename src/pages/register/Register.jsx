ace232caf04bea2485e1d6e1a6004926b27b34cd
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-03-06 16:12:13 +0600

    Register form created

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,43 @@
+import Lottie from "lottie-react";
+import React from "react";
+import registerAnimation from "../../assets/lotties/register.json";
+
+const Register = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <Lottie
+            animationData={registerAnimation}
+            loop={true}
+            style={{ width: "500px" }}
+          ></Lottie>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <h1 className="text-5xl font-bold">SignUp Now!</h1>
+            <form>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" name="email"/>
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  name="password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
