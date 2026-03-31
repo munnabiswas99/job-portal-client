a1eaa41ca364c5d98bf160437fac4593524d6202
munnabiswas99 <biswas15-5261@diu.edu.bd>
2026-03-31 23:59:20 +0600

    JWT token verified with server side and provide access to authorized users

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
+import axios from 'axios';
 
 const AuthProvider = ({children}) => {
     const [loading, setLoading] = useState(true);
@@ -31,6 +32,16 @@ const AuthProvider = ({children}) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             setLoading(false);
+            if(currentUser?.email){
+                const userData = {email: currentUser.email};
+                axios.post('http://localhost:3000/jwt', userData, {
+                    withCredentials: true
+                })
+                .then(res => {
+                    console.log(res.data)
+                })
+                .catch(error => console.log(error))
+            }
         })
         return () => {
             unSubscribe();
